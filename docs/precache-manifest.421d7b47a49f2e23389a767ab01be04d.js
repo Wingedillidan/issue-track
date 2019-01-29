ebe767befcc59b9b09a3cdabaf35418ac86030a4
Collin M <wingedillidan@gmail.com>
2019-01-29 01:58:41 -0500

    Prep for github pages

---
+++ docs/precache-manifest.421d7b47a49f2e23389a767ab01be04d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "67220ebf3cae9c6b5756",
+    "url": "/static/js/main.67220ebf.chunk.js"
+  },
+  {
+    "revision": "51536ae219a52d136c98",
+    "url": "/static/js/1.51536ae2.chunk.js"
+  },
+  {
+    "revision": "67220ebf3cae9c6b5756",
+    "url": "/static/css/main.68f5f0d6.chunk.css"
+  },
+  {
+    "revision": "30bd860fc4e22089a6a0160703cae78d",
+    "url": "/index.html"
+  }
+];
